e07756172c9ea67238901bb64d1aa1fa4d81bc0b
Prat55 <pratikdesai9059@gmail.com>
2023-08-07 15:14:22 +0530

    changes in migration folder for posts table

---
+++ database/migrations/2023_08_07_093832_alter_post_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->string('location', 100)->nullable()->default('text');
+            $table->string('date', 100)->nullable()->default('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('posts', function (Blueprint $table) {
+            $table->dropColumn('location');
+            $table->dropColumn('date');
+            $table->dropColumn('body');
+        });
+    }
+};
