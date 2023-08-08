e949603e3396e5daf47c857a1ceeec1e80fbe823
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2023-08-08 17:03:43 +0530

    changes

--- resources/views/admin/add-musiccat.blade.php
+++ resources/views/admin/add-musiccat.blade.php
@@ -10,16 +10,12 @@
                         <h2>Add New music category</h2>
                     </div>
                     <div class="card-body">
-                        <form action="/post" method="post" enctype="multipart/form-data">
+                        <form action="/add-music-cat" method="post" enctype="multipart/form-data">
                             @csrf
-                            <input type="text" name="title" class="form-control m-2" placeholder="Service Category">
-                            <input type="text" name="author" class="form-control m-2" placeholder="Title of post">
-                            <input type="text" name="location" class="form-control m-2" placeholder="location">
-                            <input type="date" name="date" class="form-control m-2">
                             <input type="url" name="link" class="form-control m-2" placeholder="link">
                             <label class="m-2">Cover Image</label>
                             <input type="file" id="input-file-now-custom-3" class="form-control m-2" name="cover">
-
+{{--
                             <div class="video-card">
                                 <label for="video" class="m-2">Play Button</label>
                                 <div class="checkbox">
@@ -31,7 +27,7 @@
                                     <input type="text" name="videolink" id="videolink"
                                         class="form-control m-2 videolink">
                                 </div>
-                            </div>
+                            </div> --}}
 
                             <button type="submit" class="btn btn-success mt-3">Submit</button>
                         </form>
