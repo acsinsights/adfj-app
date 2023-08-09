826a5fae1081f7fb69a9e98000a80f9f286c43f6
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2023-08-09 18:09:05 +0530

    changes

--- resources/views/admin/allposts.blade.php
+++ resources/views/admin/allposts.blade.php
@@ -594,15 +594,15 @@
             </div>
         </main>
         <!--[ Start:: page footer link copywrite ]-->
-        <footer class="page-footer py-4 mt-4">
+        {{-- <footer class="page-footer py-4 mt-4">
             <div class="container-fluid">
                 <p class="col-md-4 mb-0 text-muted">
                     © 2022
-                    <a href="https://pixelwibes.com/" target="_blank" title="Pixelwibes">Pixelwibes</a>, All
+                    <a href="https://pixelwibes.com/" target="_blank" title="Pixelwibes">ADFJ Studios</a>, All
                     Rights Reserved.
                 </p>
             </div>
-        </footer>
+        </footer> --}}
     </div>
 @endsection
 
