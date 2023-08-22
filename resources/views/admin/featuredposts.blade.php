1a61928d24027fcb5eb5eaea84a0130199067ca1
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2023-08-22 16:20:50 +0530

    changes

--- resources/views/admin/featuredposts.blade.php
+++ resources/views/admin/featuredposts.blade.php
@@ -108,11 +108,11 @@
                                                         <td>{{ $post->stypes->slug }}</td>
                                                         <td>{{ $post->date }}</td>
                                                         <td>{{ $post->location }}</td>
-                                                        <td><a href="/edit/{{ $post->id }}"
+                                                        <td><a href="/admin/edit/{{ $post->id }}"
                                                                 class="btn btn-outline-primary">Edit</a>
                                                         </td>
                                                         <td>
-                                                            <form action="/delete/{{ $post->id }}" method="post">
+                                                            <form action="/admin/delete/{{ $post->id }}" method="post">
                                                                 <button class="btn btn-outline-danger"
                                                                     onclick="return confirm('Are you sure?');"
                                                                     type="submit">Delete</button>
