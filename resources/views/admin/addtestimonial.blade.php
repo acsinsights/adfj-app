433ee69de2759c34b24d1d319af35db2e2dbffd4
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2023-08-23 20:21:12 +0530

    changes

--- resources/views/admin/addtestimonial.blade.php
+++ resources/views/admin/addtestimonial.blade.php
@@ -39,6 +39,41 @@
                                                         <input type="file" id="input-file-now-custom-3" class="form-control m-2"
                                                         name="cover" required>
                                                         <label class="m-2">Customer Rating</label>
+
+                                                        <section class="section">
+                                                            <div class="star-rating-bx">
+
+                                                                <div class="star-widget">
+                                                                    <form method="GET" name="feedback" action="#" onsubmit="feedBack(); return false">
+                                                                    <input type="radio" name="star" id="rate-5" value="5">
+                                                                    <label for="rate-5" class="fa-solid fa-star"></label>
+                                                                    <input type="radio" name="star" id="rate-4" value="4">
+                                                                    <label for="rate-4" class="fa-solid fa-star"></label>
+                                                                    <input type="radio" name="star" id="rate-3" value="3">
+                                                                    <label for="rate-3" class="fa-solid fa-star"></label>
+                                                                    <input type="radio" name="star" id="rate-2" value="2">
+                                                                    <label for="rate-2" class="fa-solid fa-star"></label>
+                                                                    <input type="radio" name="star" id="rate-1" value="1">
+                                                                    <label for="rate-1" class="fa-solid fa-star"></label>
+                                                                    <p id="error-rating"></p>
+                                                                    <p class="rating-desc"></p>
+
+
+                                                                        <div class="modal fade" id="simpleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                                                            <div class="modal-dialog modal-dialog-centered">
+                                                                            <div class="modal-content">
+                                                                                <div class="modal-header">
+                                                                                <h3 class="modal-title" id="exampleModalLabel">Thank you</h3>
+                                                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick="reload"></button>
+                                                                                </div>
+                                                                             
+                                                                            </div>
+                                                                            </div>
+                                                                        </div>
+                                                                    </form>
+                                                                </div>
+                                                            </div>
+                                                        </section>
                                                         {{-- <div class="testimonial__rating testimonial__rating-6">
                                                             <a href="#">
                                                                 <i class="fa-solid fa-star"></i>
