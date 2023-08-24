fd48a7386fb4dc109b570407cd2ac22db525bfef
jadhavrahulrj01 <jadhavrahulrj01@gmail.com>
2023-08-24 16:05:42 +0530

    changes

--- resources/views/admin/includes/footer.blade.php
+++ resources/views/admin/includes/footer.blade.php
@@ -2,7 +2,7 @@
     <div class="container-fluid" style="display: flex; justify-content:center; ">
         <p class="col-md-4 mb-0 text-muted">
             © 2022
-            <a href="#" target="_blank" title="ADFJ Studios">ADFJ Studios</a>, All
+            <a href="/dashboard" target="_blank" title="ADFJ Studios">ADFJ Studios</a>, All
             Rights Reserved.
         </p>
     </div>
